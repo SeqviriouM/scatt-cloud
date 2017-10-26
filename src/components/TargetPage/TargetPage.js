6f234ca7d158e7e00e8cfd9fb9898503195703d0
Roman <suffering123494@yandex.ru>
2017-10-26 13:39:19 +0300

    feat(app): added target page

---
+++ src/components/TargetPage/TargetPage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import classNames from 'classnames';
+
+import './TargetPage.scss';
+
+export default class TargetPage extends Component {
+    render() {
+        return (
+            <div className={classNames('target-page')}>
+                Target
+            </div>
+        )
+    }
+}

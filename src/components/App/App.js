5850861fbe73edb5d5fa2f556ae6f90a1fe2dcef
Roman <suffering123494@yandex.ru>
2017-10-26 13:58:40 +0300

    feat(app): added routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import classNames from 'classnames';
+
+import TargetPage from 'TargetPage/TargetPage'
 
 import './App.scss';
 
 export default class Application extends Component {
+    renderRouter() {
+        return (
+            <main className={classNames('application', 'main')}>
+                <Switch>
+                    <Route path="/target" component={TargetPage} />
+                </Switch>
+            </main>
+        );
+    }
+
     render() {
         return (
-            <div className={classNames('application')}>
-                Application
-            </div>
+            <Router>
+                <div className={classNames('application')}>
+                    Application
+                    {this.renderRouter()}
+                </div>
+            </Router>
         )
     }
 };
